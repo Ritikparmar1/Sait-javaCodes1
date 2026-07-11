1457781dd7b9a321413671f5174589765e9ba38f
HP <HP@DESKTOP-0D61HLC>
2026-07-11 12:35:00 +0530

    first

---
+++ first/src/first/printingfunction.java
@@ -0,0 +1,11 @@
+package first;
+
+public class printingfunction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int a=10,b=5;
+	        System.out.println("Values are a="+a+" and b="+b);
+	}
+
+}
